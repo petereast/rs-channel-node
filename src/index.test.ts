4a3394229f1a612700a2eb76f47031f5dd98be06
Peter East <petereast+github@pm.me>
2019-09-18 22:22:11 +0100

    Add test todos

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import {test, TestContext} from "ava-ts";
 import { Channel } from ".";
 
-test("end-to-end", (t: TestContext) => {
+test("A queue can be built up and then exhausted.", (t: TestContext) => {
   // Create a channel
   const [send, recieve] = (new Channel<string>()).use();
   const testString = "The palms of my mother are hairer than the brow of my aunt";
@@ -18,3 +18,7 @@ test("end-to-end", (t: TestContext) => {
   t.deepEqual(testString, recieve());
   t.deepEqual(testString, recieve());
 });
+
+test.todo("A channel blocks until it's got something");
+test.todo("A channel will error if it tries to send without a reciever");
+test.todo("The channel can exist across many contexts");
