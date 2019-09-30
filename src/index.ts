44c70eb2cde1f3a9838c12566fdd86f634e48034
Peter East <petereast+github@pm.me>
2019-09-30 16:03:27 +0100

    Dedupe data-stream

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,4 @@
-import { Duplex } from "readable-stream";
-
-// I need to implement my own Duplex stream
-
-class DataStream<T> extends Duplex {
-  private queue = new Array<T>();
-  constructor() {
-    super({ objectMode: true });
-  }
-
-  _write(chunk, encoding, callback) {
-    // Put a thing into a queue
-    this.queue.push(chunk);
-    // Triggers that it's time to read
-    // console.log("_write", {q: this.queue});
-    callback();
-  }
-
-  _read(_) {
-    // Read from the queue and push it into the stream
-    if (this.queue.length > 0) {
-      const chunk = this.queue.shift();
-      // console.log("_read", {chunk, q: this.queue});
-      this.push(chunk);
-    }
-  }
-}
+import { DataStream } from "./data_stream";
 
 export function channel<T>(
   maxSize: number = 100,
